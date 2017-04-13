56ba92afb53f3e567c2e37988a81be0cb86691e8
ASU5\asu5 <tbuslowski@gmail.com>
2017-04-13 22:44:13 +0200

    home page, routes and voting functionality - beginning

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,11 @@
 class WelcomeController < ApplicationController
   def index
+    @elections = Election.where("start < ?", (Time.now))
+
   end
+
+  def vote
+    @election = Election.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :elections
   root 'welcome#index'
 
+  get '/vote/:id' => 'welcome#vote', as: 'vote'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
