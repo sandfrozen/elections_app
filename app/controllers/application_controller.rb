c021ad34fa05a59feb08f458fda0ef2bb6a46d19
ASU5\asu5 <tbuslowski@gmail.com>
2017-04-16 14:52:01 +0200

    user registration and authentication upgraded

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
   protect_from_forgery with: :exception
 
+
+  def authenticate_admin!
+    redirect_to main_app.root_path unless current_user.admin? == true
+  end
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname, :pesel])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname, :pesel])
   end
 
