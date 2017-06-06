844900944c16a06e2ce7fa9ae392d0dda71a014e
Szymon Bakunowicz <zorzin0os@gmail.com>
2017-06-07 01:17:45 +0200

    Vote and results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -9,9 +9,8 @@ class WelcomeController < ApplicationController
   end
 
   def vote
-    @isUserPermission = ElectionUser.where("User_id = ? and Election_id = ?", current_user.id, params[:id])
-
-    if @isUserPermission.present?
+    @isUserPermission = ElectionUser.where("User_id = ? and Election_id = ? and vote is ?", current_user.id, params[:id], nil).to_a
+    if @isUserPermission.length >0
       @election = Election.find(params[:id])
     else
       respond_to do |format|
@@ -23,15 +22,21 @@ class WelcomeController < ApplicationController
 
   def save_vote
     candidates = params[:candidate]
-      for candidate in candidates
+    old_users = ElectionUser.where("User_id = ? and Election_id = ? and vote is ?", current_user.id, params[:election_id], nil).to_a
+    old_users.each { |old_user|
+      old_user.destroy
+    }
+
+    max = params[:max_votes].to_i
+    (0..candidates.length-1).each { |i|
+      if i < max
         el_user = ElectionUser.new
         el_user.User = User.find(current_user.id)
         el_user.Election = Election.find(params[:election_id])
-        el_user.vote = candidate
-        puts candidate
+        el_user.vote = candidates[i]
         el_user.save
-        puts el_user.errors.full_messages
       end
+    }
       respond_to do |format|
         format.html { redirect_to root_path, warning: 'Głos oddany' }
         format.json { render :show, status: :created, location: @election }
@@ -40,6 +45,11 @@ class WelcomeController < ApplicationController
 
   def election_result
     @election = Election.find(params[:id])
+    @election_voters = ElectionUser.select(:User_id).where("Election_id = ?",params[:id]).uniq.to_a
+    @election_votes = ElectionUser.select(:User_id).where("Election_id = ? and vote is not null",params[:id]).uniq.to_a
+    @frequency = @election_votes.length.to_f / @election_voters.length.to_f * 100
+    @results = ElectionUser.group(:vote).where("Election_id = ?",params[:id]).order('count_all desc').count.to_a
+    @candidates = User.all
   end
 
   def election_info
